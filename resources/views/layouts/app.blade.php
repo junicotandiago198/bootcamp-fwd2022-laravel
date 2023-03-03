097b1860c04373b10a9d3f713b7ef8919f47075c
junicotandiago198 <junicotandiagoo21@gmail.com>
2023-03-03 11:28:05 +0700

    slicing view frontsite landingpage, appointment and payment

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,11 +13,9 @@
         <!-- Styles -->
         @livewireStyles
 
-        <!-- Styles -->
-        <link rel="stylesheet" href="{{ mix('css/app.css') }}">
-
         <!-- Scripts -->
-        <script src="{{ mix('js/app.js') }}" defer></script>
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+        <script src="{{ asset('js/app.js') }}" defer></script>
     </head>
     <body class="font-sans antialiased">
         <x-jet-banner />
